6909095ef0b3861a173a799297bddcf7006c2dc4
vk2gpz <vk2gpz@gmail.com>
2019-01-08 14:42:55 +1100

    removed import statements.

--- src/main/java/com/vk2gpz/nomorehunger/NoMoreHunger.java
+++ src/main/java/com/vk2gpz/nomorehunger/NoMoreHunger.java
@@ -1,12 +1,11 @@
 package com.vk2gpz.nomorehunger;
-import org.bukkit.event.*;
 public class NoMoreHunger extends org.bukkit.plugin.java.JavaPlugin implements org.bukkit.event.Listener {
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 	
-	@EventHandler(priority = EventPriority.MONITOR)
+	@org.bukkit.event.EventHandler(priority = org.bukkit.event.EventPriority.MONITOR)
 	public void onHunger(org.bukkit.event.entity.FoodLevelChangeEvent e) {
 		if (e.getEntity().hasPermission("nomorehunger")) {
 			e.setFoodLevel(20);  // set it full, in case...
